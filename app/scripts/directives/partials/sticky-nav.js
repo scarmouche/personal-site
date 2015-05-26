ed55d8ec952286a73a8410f176f22e087b6a62ad
Selena Carmouche <scarmouche@dustland.com>
2015-05-25 23:04:28 -0700

    Stick nav functions on mobile as intended, added in header component within the layout

--- app/scripts/directives/partials/sticky-nav.js
+++ app/scripts/directives/partials/sticky-nav.js
@@ -5,13 +5,23 @@ Sticky Nav Directive
 **/
 
 angular.module('smouche')
-  .directive('stickyNav', [ function () {
+  .directive('stickyNav', ['$window', function ($window) {
     return {
       restrict: 'E',
       replace: false,
       templateUrl: '/views/partials/sticky-nav.html',
       link: function(scope, element, attr) {
 
+        angular.element($window).bind('scroll', function() {
+
+          if ($window.scrollY < 100 && element[0].style.position !== 'fixed') {
+            scope.isStuck = false;
+          } else {
+            scope.isStuck = true;
+          }
+          scope.$apply();
+
+        });
       }
     };
   }]);
